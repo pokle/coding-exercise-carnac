8c472cf90e49174870060553b0099286b7637c06
Tushar Pokle <tushar.pokle@gmail.com>
2015-05-11 22:22:02 +1000

    Ah! The trouble was combine

--- carnac.hs
+++ carnac.hs
@@ -1,27 +1,24 @@
 
 digits = [1..9]
 combine a b = a * 10 + b
-ops = [((+),"+"), ((-),"-"), (combine,"")]
+ops = [((+),"+")
+      , ((-),"-")
+      , (combine,"")
+      ]
 
 
 carnac 1 = [ (show digit, digit) | digit <- digits ]
 
---carnac 2 = 
---    [ (show d1 ++ opname ++ show d2, d1 `op` d2) 
---    | d1 <- digits
---    , d2 <- digits
---    , (op, opname) <- ops
---    ]
-
-
 carnac n =
-    [ (instructions ++ opname ++ show digit, accumulator `op` digit) 
+    [ (instructions ++ opname ++ show digit, op accumulator digit) 
     | (instructions, accumulator) <- carnac (n - 1)
     , (op, opname) <- ops
     , digit <- digits
     ]
 
-carnac_goal digs goal = 
-    [ (instructions,accumulator) 
-    | (instructions, accumulator) <- carnac digs
+carnac_goal digits goal = 
+    [ instructions 
+    | (instructions, accumulator) <- carnac digits
     , goal == accumulator ]
+
+main = putStr $ unlines $ carnac_goal 2 100
