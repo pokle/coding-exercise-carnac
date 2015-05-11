56da8aac6bec6888e85f5f297cd4b3c823dcc723
Tushar Pokle <tushar.pokle@gmail.com>
2015-05-11 23:45:54 +1000

    calc trouble

--- carnac.hs
+++ carnac.hs
@@ -1,37 +1,29 @@
 
-digits = [1..9]
-combine a b = a * 10 + b
-ops = [((+),"+")
-      , ((-),"-")
-      , (combine,"")
-      ]
-
---- Misinterpretation
-
-carnac 1 = [ (show digit, digit) | digit <- digits ]
-
-carnac n =
-    [ (instructions ++ opname ++ show digit, op accumulator digit) 
-    | (instructions, accumulator) <- carnac (n - 1)
-    , (op, opname) <- ops
-    , digit <- digits
-    ]
-
-carnac_goal digits goal = 
-    [ instructions 
-    | (instructions, accumulator) <- carnac digits
-    , goal == accumulator ]
-
-
----
-
 cn :: Int -> [String]
 cn n 
    | n < 2     = ["1"]
    | otherwise = [ prefix ++ op ++ show n
                  | prefix <- cn (n - 1)
-                 , op <- ["+", "-", ""]
+                 , op <- [" + ", " - ", ""]
                  ]
 
 
-main = putStr $ unlines $ carnac_goal 2 100
+calc :: String -> Int
+calc s = calcWords (words s)
+
+calcWords :: [String] -> Int
+calcWords [] = 0
+calcWords [s] = (read s :: Int)
+--calcWords (lhs:"+":rhs:rest) = (read lhs :: Int) + (read rhs :: Int) + (calcWords rest)
+--calcWords (lhs:"-":rhs:rest) = (read lhs :: Int) - (read rhs :: Int) + (calcWords rest)
+calcWords (lhs:op:rhs:rest)
+          | op == "+" = (read lhs :: Int) + (read rhs :: Int) + (calcWords rest)
+          | op == "-" = (read lhs :: Int) - (read rhs :: Int) + (calcWords rest)
+
+
+carnac n = [ x ++ " == " ++ show (calc x) 
+           | x <- cn n
+           , (calc x) == 100 
+           ]
+
+main = putStr $ unlines $ carnac 9
